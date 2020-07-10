126c5669aa11fb87f4d464c1eec109cd860eeb50
siamor9724 <56284745+siamor9724@users.noreply.github.com>
2020-07-10 19:25:53 +0900

    Add files via upload

---
+++ BossController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Net;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BossController : EnemyBase
+{
+
+    static BossController instance;
+    public static BossController Instance
+    {
+        get
+        {
+            return instance;
+        }
+    }
+    void Awake() { instance = this; }
+    public BossRoomCondition bossRoom;
+    private void Start()
+    {
+        color = gameObject.GetComponent<SpriteRenderer>().color;
+        if (gameObject.transform.parent == null) return;
+        bossRoom = gameObject.transform.parent.GetComponent<BossRoomCondition>();
+    }
+  /*  private void Update()
+    {
+       
+    }*/
+   
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        
+    }
+  
+
+   
+
+}
---
+++ StopBehavior.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StopBehavior : StateMachineBehaviour
+{
+    private int rand;
+
+    //start
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+
+        rand = Random.Range(0, 3);
+
+        if (rand == 0)
+        {
+            animator.SetTrigger("IsLongDistanceAttack");
+        }
+        else if (rand == 1)
+        {
+            animator.SetTrigger("IsWalk");
+        }
+        else
+        {
+            animator.SetTrigger("IsRushAttack");
+        }
+    }
+
+    //update
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+
+    }
+
+    //한번만 실행
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+
+    }
+
+}
